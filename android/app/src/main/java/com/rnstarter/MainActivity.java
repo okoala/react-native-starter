d60c9c014780e7ad36384369a4956b4748e0fbbd
okoala <dapixp@gmail.com>
2016-02-22 11:20:25 +0800

    react-native upgrade

--- android/app/src/main/java/com/rnstarter/MainActivity.java
+++ android/app/src/main/java/com/rnstarter/MainActivity.java
@@ -1,9 +1,10 @@
 package com.rnstarter;
 
 import com.facebook.react.ReactActivity;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.babisoft.ReactNativeLocalization.ReactNativeLocalizationPackage;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
-import com.babisoft.ReactNativeLocalization.ReactNativeLocalizationPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -36,6 +37,7 @@ public class MainActivity extends ReactActivity {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
         new MainReactPackage(),
+        new VectorIconsPackage(),
         new ReactNativeLocalizationPackage()
       );
     }
